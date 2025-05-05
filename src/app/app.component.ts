345615fe1c35042a042615885303d83eb1fc698a
Avila-Pato <p.avilaf1998@gmail.com>
2025-05-05 16:14:25 -0400

    angular proyecto + nesjt improving webDevelopment idontkow waht can i write

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,  NavbarComponent, CommonModule],
+  standalone: true, 
+  templateUrl: './app.component.html',  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/shared/top-nabvar/top-nabvar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-nabvar',
+  imports: [],
+  templateUrl: './top-nabvar.component.html',
+  styleUrl: './top-nabvar.component.css'
+})
+export class TopNabvarComponent {
+
+}
