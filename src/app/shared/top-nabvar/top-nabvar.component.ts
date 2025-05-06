48264dfd34398e9c6ec5544138200fafcbc55f78
Avila-Pato <p.avilaf1998@gmail.com>
2025-05-06 16:46:03 -0400

    Refactor code structure for improved readability and maintainability

--- src/app/shared/top-nabvar/top-nabvar.component.ts
+++ src/app/shared/top-nabvar/top-nabvar.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-top-nabvar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './top-nabvar.component.html',
   styleUrl: './top-nabvar.component.css'
 })
 export class TopNabvarComponent {
+  isScrolled = false;
+  showNavbar = true;
+  lastScrollTop = 0;
 
+  @HostListener('window:scroll', [])
+  
+  onWindowScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    this.isScrolled = scrollTop > 100;
+
+    const scrollDelta = scrollTop - this.lastScrollTop;
+
+    if(scrollDelta > 20) {
+      this.showNavbar = false;
+    } else if (scrollDelta < -20 && scrollTop < document.body.scrollHeight * 0.7) {
+      this.showNavbar = true;
+    }
+
+    this.lastScrollTop = scrollTop;
+  }
 }
